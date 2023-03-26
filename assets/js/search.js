a1ec2c832635bb95aa0b2bca35c8740fd48c17c8
shruthim0 <shruthim0@users.noreply.github.com>
2023-03-26 23:52:29 +0000

    deploy: 55ce07d49ee85aca0130e699dfdf2a4bb14b3906

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/datapirates/assets/js/search-data.json', true);
+    request.open('GET', '/canada/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
